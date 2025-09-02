8f652e4bfffbecb26c4a5ad91c3d42b87bf845c0
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-02 16:01:51 +0530

    Created Footer1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-9xl">From App</h1>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import Footer1 from "../components/footer/Footer1"
+
+
+
+const Home = () => {
+  return (
+    <>  
+      
+          <Footer1 />
+     
+        
+    </>
+  )
+}
+
+export default Home
