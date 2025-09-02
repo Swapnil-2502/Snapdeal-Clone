4d95f18dadad8a363c2016e81b06e58a71032915
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-02 16:46:07 +0530

    Footer Middle Top added

--- frontend/src/components/footer/footerbottom/FooterMiddleTop.tsx
+++ frontend/src/components/footer/footerbottom/FooterMiddleTop.tsx
@@ -3,7 +3,61 @@
 export const FooterMiddleTop = () => {
   return (
     <>
-    
+      <div className="middleTop row">
+ 		    <div className="footer-inner">
+ 		        <div className="col-xs-5">
+                    <div className="middle-heading">Policy Info</div>
+                    <ul className="middle-content">
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/page/privacy-policy">Privacy Policy</a></li>
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/page/terms-of-sale">Terms of Sale</a></li>
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/offers/terms-of-use">Terms of Use</a></li>
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/page/abusePolicy">Report Abuse &amp; Takedown Policy</a></li>
+                        <li><a className="underLineOnHover" href="https://www.services.bis.gov.in/php/BIS_2.0/bisconnect/knowyourstandards/indian_standards/isdetails/">Know Your BIS Standard</a></li>
+                        <li><a className="underLineOnHover" href="https://www.bis.gov.in/products-under-compulsory-certification/">Products Under Cumpulsory BIS Certification</a></li>
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/faq/faqhome">FAQ</a></li>
+                    </ul>
+                </div>
+                <div className="col-xs-4">
+                    <div className="middle-heading">Company</div>
+                    <ul className="middle-content">
+                        <li><a className="underLineOnHover" href="http://www.snapdeal.com/page/about-us">About Us</a></li>
+                        <li><a className="underLineOnHover" href="https://snapdeal.darwinbox.in/ms/candidate/careers">Careers</a></li>
+                        <li><a className="underLineOnHover" href="http://blog.snapdeal.com/">Blog</a></li>
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/info/sitemap">Sitemap</a></li>
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/faq/faqCustomerCare">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div className="col-xs-4">
+                    <div className="middle-heading">Snapdeal Business</div>
+                    <ul className="middle-content">
+                        <li><a className="underLineOnHover" href="https://play.google.com/store/apps/details?id=com.snapdeal.main&amp;hl=en_IN">Shopping App</a></li>
+                        <li><a className="underLineOnHover" href="http://sellers.snapdeal.com/">Sell on Snapdeal</a></li>
+                        <li><a className="underLineOnHover" href="mailto:mediarelations@snapdeal.com">Media Enquiries</a></li>
+                    </ul>
+                </div>
+                <div className="col-xs-4">
+                    <div className="middle-heading">Popular Links</div>
+                    <ul className="middle-content">
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/products/women-apparel-lehenga">Lehenga</a></li>
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/products/kids-clothing">Kid's Clothing</a></li>
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/products/women-apparel-sarees">Sarees</a></li>
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/products/mens-winter-wear-store">Winter Wear</a></li>
+                        <li><a className="underLineOnHover" href="https://www.snapdeal.com/products/men-apparel-sweatshirts">Sweatshirts</a></li>
+                    </ul>
+                    <i className="originalalityfooterIcon footerIconsImg hide"></i>
+                </div>
+                <div className="col-xs-6 row">
+                  <div className="middle-heading subscribeHead">Subscribe</div>
+                  <input name="keyword" type="text" className="col-xs-15 subscribeInputBox" placeholder="Your email address"/>
+                  <button className="col-xs-7 btn btn-theme-secondary subscribeBtn reset-padding">Subscribe</button>
+                  <div className="sucessfulSubscribe hidden">Succesfully Subscribed</div>
+                  <div className="errorSubscribe hidden">Something went wrong. Please try again!</div>
+                  <div className="errorSubscribeEmail hidden"> Please enter a valid email address.</div>
+                  <p className="registerSubscribeText col-xs-24 mt12 reset-padding">Register now to get updates on promotions and </p>
+                  <p className="registerSubscribeText col-xs-24 reset-padding">coupons. <span><a href="https://play.google.com/store/apps/details?id=com.snapdeal.main">Or Download App</a></span></p>
+                </div>
+          </div>
+      </div>
     </>
   )
 }
