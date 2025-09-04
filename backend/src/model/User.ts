66b03bdf4461ebe4ee0847195099f6b19d8c89ab
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-04 20:05:13 +0530

    minor changes

--- backend/src/model/User.ts
+++ backend/src/model/User.ts
@@ -1,21 +1,22 @@
 import mongoose, {Schema} from "mongoose";
 
 export interface IUser extends Document {
-    phone: string,
     email: string,
+    phone: string,
     name: string,
     dob: string,
-    password: string
+    password: string,
+    isVerified: boolean
 } 
 
 const UserSchema: Schema = new Schema<IUser>({
-
-    phone:{type: String, required: true, unique:true },
     email:{type: String, required: true, unique:true },
+    phone:{type: String, required: true, unique:true },
     name:{type: String, required: true},
     dob:{type: String, required: true},
-    password:{type: String, required: true}
-    
+    password:{type: String, required: true},
+    isVerified:{type: Boolean, default: false}
+
 },{timestamps:true})
 
 export default mongoose.model<IUser>("users",UserSchema)
