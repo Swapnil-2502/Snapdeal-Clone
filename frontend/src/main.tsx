7cdbec1622492349d78cca807be682e21d562112
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-07 16:22:54 +0530

    Wrapping Context to Main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+
+//Importing CSS files
+import '../public/clone-styles/style1.css'
+import '../public/clone-styles/style2.css'
+// import '../public/clone-styles/style3.css'
+// import '../public/clone-styles/style4.css'
+// import '../public/clone-styles/style5.css'
+import '../public/clone-styles/style7.css'
+import { AuthProvider } from './contexts/AuthContext.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
