448a60172cc265e36115a872784e1893f823def6
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-08 16:02:53 +0530

    Added /Home Components

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,25 @@
 import { FooterBottom } from "../components/footer/FooterBottom"
 import FooterTop from "../components/footer/FooterTop"
+import { Carousel } from "../components/Home/Carousel"
+import { LeftSideNavbar } from "../components/Home/LeftSideNavbar"
+import { Poster } from "../components/Home/Poster"
 
 
 
 const Home = () => {
   return (
     <> 
+   
+    <div id="content_wrapper">
+      <LeftSideNavbar />
+      <Carousel />
+      <Poster />
+    </div>
+    
     <div className="mt-96">
       <div id="sdFooter">
-          <FooterTop />
-          <FooterBottom />
+        <FooterTop />
+        <FooterBottom />
       </div>
     </div>
       
