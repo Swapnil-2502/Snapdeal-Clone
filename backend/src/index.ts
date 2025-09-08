3384b83ccda00aa7de216fb9acad5157aababb01
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-09 02:37:49 +0530

    Added Product routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import cors from "cors"
 import { ConnectDB } from "./config/db"
 import authRoutes from "./routes/authRoute"
+import productRoutes from "./routes/productRoute"
 
 dotenv.config()
 
@@ -18,6 +19,7 @@ app.use(cors({
 app.use(express.json())
 
 app.use('/api/auth',authRoutes)
+app.use('/api/product',productRoutes)
 
 
 app.get("/",(req: Request,res: Response)=>{
