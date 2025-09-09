7a99810d65b47622f0abfadf498cd7498dae9408
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-09 19:48:13 +0530

    Wrapped CartContext & added CSS files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,14 +11,19 @@ import '../public/clone-styles/style2.css'
 // import '../public/clone-styles/style4.css'
 // import '../public/clone-styles/style5.css'
 import '../public/clone-styles/style7.css'
+import '../public/clone-styles/style9.css'
+import '../public/clone-styles/style10.css'
 import { AuthProvider } from './contexts/AuthContext.tsx'
+import { CartProvider } from './contexts/CartContext.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
