02080b1aaad6c88bf0a3a2fbed47c63ea48ee749
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-11 02:19:07 +0530

    Routing done for Account section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,27 @@ import Home from './pages/Home'
 import { ProductPage } from './pages/ProductPage'
 import { AddToCartPage } from './pages/AddToCartPage'
 
-function App() {
+import { MyAccount } from './pages/MyAccount'
+import { Orders } from './components/Account/Orders'
+import { SavedAddresses } from './components/Account/SavedAddresses'
+import { CartSection } from './components/Cart/CartSection'
 
+function App() {
   return (
     <>
-      <Header />
+      <div id='sdHeader' className='comp-header reset-padding'>
+        <Header />
+      </div>
+      <CartSection/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/product/:productTitle/:productId' element={<ProductPage />} />
         <Route path='/cart/addTocart/:productId' element={<AddToCartPage />} />
+        <Route path='/myaccount/' element={<MyAccount />}>
+            <Route path="myorders" element={<Orders />} />
+            <Route path="savedAddresses" element={<SavedAddresses />} />
+        </Route>
+
       </Routes>
     </>
   )
