34bc48e2d167173563f240534acc093ece0f4c88
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-14 15:37:33 +0530

    Order Details Page

---
+++ frontend/src/components/Account/OrderDetail.tsx
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import axios from "../../api/axios"
+import type { OrderType } from "../../types/types"
+
+
+export const OrderDetail = () => {
+	const {orderId} = useParams()
+	const [orderDetails, setOrderDetails] = useState<OrderType>()
+	
+	const UserData = JSON.parse(localStorage.getItem("UserData") || "{}")
+	const token = localStorage.getItem("Token")
+	const headers = {
+		Authorization: `Bearer ${token}`,
+	}
+	
+	useEffect(() => {
+		const fetchOrders = async () => {
+			const orders = await axios.get(`/order/${orderId}`,{headers})
+			setOrderDetails(orders.data.order)
+		}
+		fetchOrders()
+	},[])
+
+	const generateSlug = (title: string) => {
+		return title.toLowerCase().replace(/[^\w\s]/gi, '').replace(/\s+/g, '-').substring(0, 60); 
+	}
+
+
+  return (
+    <>
+        <div className="ordersummarybox">
+		    	<span className="accountOrder">ORDER DETAILS</span>
+		   
+		    	<div className="ordHeaderDetails">
+					<span className="orderNo">Order ID: {orderDetails?._id}  <span className="orderNo">(2 Items)</span></span>
+				</div>
+
+			    <div className="customerInfomation  ">
+			    	<span className="customerEmail">Customer Information<div className="cusEmail"><span className="cusmail">Email</span>: {UserData.email}</div></span>
+			    		
+						<span className="priceDetails">
+								<div><span className="amountDetails">Total&nbsp;&nbsp;<i className="detColon">:</i></span><span className="amntVal"> ₹ {orderDetails?.totalAmount}</span></div>
+						    	<div><span className="amountDetails">Delivery Charges&nbsp;&nbsp;<i className="detColon">:</i></span><span className="amntVal"> FREE </span></div>
+								<div className="paymentDiv">
+                                	<div><span className="amountDetails amtPaid">Payable Amount&nbsp;&nbsp;<i className="detColon">:</i></span><span className="amntVal amtPaid"> ₹ {orderDetails?.totalAmount}</span></div>
+                                </div>							   	    
+					    </span>
+
+					<span className="paymentMethod">Payment Method<div className="ordCOD">Online</div></span>
+				</div>
+			    
+			   	{orderDetails?.items.map((item,index) => {
+					const slug = generateSlug(item.title)
+					return (
+				<div key={index} className="suborderSummary">
+											 	
+					<div className="subOrdSumDetails">
+						<span className="orderNo subOrdId">Suborder Id: {item._id}</span>
+									
+						<div className="subOrdTopActions">	
+							<span className="subordSumImage">
+								
+								<Link to={`/product/${slug}/${item._id}`}  className="subOrderImage">
+									<img alt={item.title} title={item.title} className="gridViewImage" data-border={0} src={item.imageURL}/>
+								</Link>
+							</span>
+								    <span className="subOrdSumAction ">
+										<div className="subOrderItemNo">Item {index+1}</div>
+								   		<Link to={`/product/${slug}/${item._id}`} className="subOrdName subOrdSumaryName"   ><div style={{paddingTop: "10px"}}>{item.title}</div></Link>
+										<div className="subOrderItemNo" style={{paddingTop: "20px"}}>Quantity: {item.quantity}</div>
+										<div className="subOrderItemNo" style={{paddingTop: "20px"}}>Unit Price: ₹ {item.price}</div>
+					
+									</span>
+									<span className="subOrdPriceDetails">
+										<div className="totalAmntOuter"><span className="amountDetails totalAmountDetails"  style={{fontSize:"18px"}}>₹</span><span className="amntClt" style={{fontSize:"18px"}}>{item.price * item.quantity}</span><span className="signCircle "></span></div>
+										
+									</span>
+						</div>
+														   
+						<div className="trackingDetails  bottomBorderTrack">									        					 
+							<span className="subOrdStatus">Status: <span className="subOrdStatusText">ORDER CONFIRMED</span></span>
+							<span className="expectDel ">Est. Delivery: <span className="subOrdStatusText  "> 2-3 Days</span></span>					            
+						            		
+							
+						</div>					
+						
+						<div className="subOrdTimeLine">
+								<span className="statusCircle statusCir4 greenStatusCircle"></span>
+							<span className="statusLine statusLine4"></span> 
+								<span className="statusCircle statusCir4"></span>
+							<span className="statusLine statusLine4"></span> 
+								<span className="statusCircle statusCir4"></span>
+							<span className="statusLine statusLine4"></span> 
+								<span className="statusCircle statusCir4"></span>
+							<span className="statusLine statusLine4"></span> 
+								<span className="statusCircle statusCir4"></span>
+						</div>
+
+						<div className="timeLineText">
+							<span className="timelineStatusText  timelineText5">Placed</span>
+							<span className="timelineStatusText  timelineText5">Packed</span>
+							<span className="timelineStatusText  timelineText5">Shipped</span>
+							<span className="timelineStatusText  timelineText5">Out for delivery</span>
+							<span className="timelineStatusText  timelineText5">Delivered</span>
+						</div>
+				
+													
+						
+										   		
+						<div className="treak-btn js-real-track-timeline btn btn-theme-secondary btn-line">TRACK</div>
+                        <div className="promotionalBtn "></div>
+                        <div className="deliverySellerDetails">
+                            <span className="deliveryDet">
+                                <span className="delHead">Shipping Information</span><br/>
+                                <span className="delName" style={{paddingBottom:"8px"}}>{orderDetails.address.name},</span>
+                                <span className="delContent">{orderDetails.address.address}, {orderDetails.address.landmark}, {orderDetails.address.city}</span>
+                                <span className="delContent">{orderDetails.address.state} -{orderDetails.address.pincode}</span> 
+                                <span className="mobDetails padB5">Mobile No: <span className="delMob">{orderDetails.address.mobileNumber}</span></span>
+                            </span> 
+                        </div>		
+														
+					</div>
+				</div>)
+				})}
+	    </div>
+    </>
+  )
+}
