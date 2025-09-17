5a4a0f8bbe37b817a579d00146c0ec68e01e11c3
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-18 02:16:55 +0530

    Added Customer Ratings

--- frontend/src/components/Products/ListProducts/LeftFilters.tsx
+++ frontend/src/components/Products/ListProducts/LeftFilters.tsx
@@ -1,14 +1,9 @@
 import { useState } from "react";
-import type { ProductFilter } from "../../../types/types"
+import { useProductFilters } from "../../../contexts/ProductFilterContext";
 
-interface LeftFiltersProps {
-  filters: ProductFilter
-  setFilters:  React.Dispatch<React.SetStateAction<ProductFilter>>;
-}
-
-
-export const LeftFilters: React.FC<LeftFiltersProps> = ({filters, setFilters}) => {
+export const LeftFilters = () => {
     const [priceRange, setPriceRange] = useState({ min: "", max: "" });
+    const {filters, setFilters} = useProductFilters()
 
     const applyPriceFilter = () => {
         setFilters({
@@ -58,6 +53,84 @@ export const LeftFilters: React.FC<LeftFiltersProps> = ({filters, setFilters}) =
                                 </div>
                         </div>
 
+                        <div className="filter-section " data-filtername="avgRating" data-displayname="Customer Rating" data-isvisible="true">
+
+                            <div className="filter-name-section filter-type clearfix">
+                                <div className="filter-type-name lfloat">
+                                    Customer Rating
+                                    <span className="filter-clear hidden" data-displaytype="radio">
+                                        Clear
+                                    </span>
+                                </div>
+                            </div>
+                
+                            <div className="filter-content" style={{height: "auto"}}>
+                                <div className="filter-inner ">
+                                    
+                                    <div className="sdRadio filters-list">
+                                            <input id="avgRating-4.0" className="filter-value visible-filter" type="radio" checked={filters.minStars === '4'} value="4" onChange={(e) => e.target.checked ? setFilters({...filters, minStars: '4'}) : setFilters({...filters,minStars: ''})}/> 
+                                            <label htmlFor="avgRating-4.0" style={{width: "240px"}}> 
+                                                
+                                                <a className="filter-name " style={{paddingLeft:'10px'}}>
+                                                    <div className="rating-stars ">
+                                                        <div className="grey-stars"></div>
+                                                        <div className="filled-stars" style={{width:"80.0%"}}></div>
+                                                        
+                                                    </div>
+                                                    <span className="more-rating padL15">&amp; Up</span>		
+                                                </a> 
+                                               
+                                            </label>
+                                    </div>
+                                    <div className="sdRadio filters-list">
+                                            <input id="avgRating-3.0" className="filter-value visible-filter" type="radio" checked={filters.minStars === '3'} value="3" onChange={(e) => e.target.checked ? setFilters({...filters, minStars: '3'}) : setFilters({...filters,minStars: ''})}/> 
+                                            <label htmlFor="avgRating-3.0" style={{width: "240px"}}> 
+                                                
+                                                <a className="filter-name " style={{paddingLeft:'10px'}}>
+                                                    <div className="rating-stars ">
+                                                        <div className="grey-stars"></div>
+                                                        <div className="filled-stars" style={{width:"60.0%"}}></div>
+                                                        
+                                                    </div>
+                                                    <span className="more-rating padL15">&amp; Up</span>		
+                                                </a> 
+                                               
+                                            </label>
+                                    </div>
+                                    <div className="sdRadio filters-list">
+                                            <input id="avgRating-2.0" className="filter-value visible-filter" type="radio" value="2" checked={filters.minStars === '2'} onChange={(e) => e.target.checked ? setFilters({...filters, minStars: '2'}) : setFilters({...filters,minStars: ''})}/> 
+                                            <label htmlFor="avgRating-2.0" style={{width: "240px"}}> 
+                                                
+                                                <a className="filter-name " style={{paddingLeft:'10px'}}>
+                                                    <div className="rating-stars ">
+                                                        <div className="grey-stars"></div>
+                                                        <div className="filled-stars" style={{width:"40.0%"}}></div>
+                                                        
+                                                    </div>
+                                                    <span className="more-rating padL15">&amp; Up</span>		
+                                                </a> 
+                                               
+                                            </label>
+                                    </div>
+                                    <div className="sdRadio filters-list">
+                                            <input id="avgRating-1.0" className="filter-value visible-filter" type="radio" value="1" checked={filters.minStars === '1'} onChange={(e) => e.target.checked ? setFilters({...filters, minStars: '1'}) : setFilters({...filters,minStars: ''})}/> 
+                                            <label htmlFor="avgRating-1.0" style={{width: "240px"}}> 
+                                                
+                                                <a className="filter-name " style={{paddingLeft:'10px'}}>
+                                                    <div className="rating-stars ">
+                                                        <div className="grey-stars"></div>
+                                                        <div className="filled-stars" style={{width:"20.0%"}}></div>
+                                                        
+                                                    </div>
+                                                    <span className="more-rating padL15">&amp; Up</span>		
+                                                </a> 
+                                               
+                                            </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                         <div className="filter-section " data-filtername="Color_s" data-displayname="Color" data-isvisible="true">
 
                             <div className="filter-name-section filter-type clearfix">
@@ -73,40 +146,40 @@ export const LeftFilters: React.FC<LeftFiltersProps> = ({filters, setFilters}) =
                             
                             <div className="filter-content ">
                                 <div className="filter-inner filter-inner-height">
-                                    <div className=" filters-list ">
+                                    <div className="sdCheckbox filters-list ">
                                         <input id="Color_s-Black" type="checkbox" checked={filters.color === 'Black'} onChange={(e) => e.target.checked ? setFilters({...filters,color: "Black"}) :   setFilters({...filters,color: ""})} className="filter-value visible-filter" value="Black"/>
                                         <label htmlFor="Color_s-Black">
                                             <span className="filter-color-tile Black " style={{background: "Black", marginLeft:'20px'}}></span> 
-                                            <span className="filter-name hashAdded"> Black</span>
+                                            <span className="filter-name "> Black</span>
                                         </label>
                                     </div>
 
-                                    <div className=" filters-list ">
+                                    <div className="sdCheckbox filters-list ">
                                         <input id="Color_s-Blue" type="checkbox" checked={filters.color === 'Blue'} onChange={(e) => e.target.checked ? setFilters({...filters,color: "Blue"}) :   setFilters({...filters,color: ""})} className="filter-value visible-filter" value="Blue"/>
                                         <label htmlFor="Color_s-Blue">
                                             <span className="filter-color-tile Blue "style={{background: "Blue",marginLeft:'20px'}}></span> 
-                                            <a className="filter-name hashAdded"> Blue</a>
+                                            <a className="filter-name "> Blue</a>
                                         </label>
                                     </div>
-                                    <div className=" filters-list ">
+                                    <div className="sdCheckbox filters-list ">
                                         <input id="Color_s-Brown" type="checkbox" checked={filters.color === 'Brown'} onChange={(e) => e.target.checked ? setFilters({...filters,color: "Brown"}) :   setFilters({...filters,color: ""})} className="filter-value visible-filter" value="Brown"/>
                                         <label htmlFor="Color_s-Brown">
                                             <span className="filter-color-tile Brown "style={{background: "Brown",marginLeft:'20px'}}></span> 
-                                            <a className="filter-name hashAdded"> Brown</a>
+                                            <a className="filter-name "> Brown</a>
                                         </label>
                                     </div>
-                                    <div className=" filters-list ">
+                                    <div className="sdCheckbox filters-list ">
                                         <input id="Color_s-Green" type="checkbox" checked={filters.color === 'Green'} onChange={(e) => e.target.checked ? setFilters({...filters,color: "Green"}) :   setFilters({...filters,color: ""})} className="filter-value visible-filter" value="Green"/>
                                         <label htmlFor="Color_s-Green">
                                             <span className="filter-color-tile Green "style={{background: "Green",marginLeft:'20px'}}></span> 
-                                            <a className="filter-name hashAdded"> Green</a>
+                                            <a className="filter-name "> Green</a>
                                         </label>
                                     </div>
-                                    <div className=" filters-list ">
+                                    <div className="sdCheckbox filters-list ">
                                         <input id="Color_s-Orange" type="checkbox" checked={filters.color === 'Orange'} onChange={(e) => e.target.checked ? setFilters({...filters,color: "Orange"}) :   setFilters({...filters,color: ""})} className="filter-value visible-filter" value="Orange"/>
                                         <label htmlFor="Color_s-Orange">
                                             <span className="filter-color-tile Orange "style={{background: "Orange", marginLeft:'20px'}}></span> 
-                                            <a className="filter-name hashAdded"> Orange</a>
+                                            <a className="filter-name "> Orange</a>
                                         </label>
                                     </div>
                                     
