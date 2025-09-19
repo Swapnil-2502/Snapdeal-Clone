9c91955e32e047d66407cde4cf563cad5a60287b
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-19 11:50:30 +0530

    Listing Products from Search

--- frontend/src/components/Products/ListProducts/ListProduct.tsx
+++ frontend/src/components/Products/ListProducts/ListProduct.tsx
@@ -2,33 +2,50 @@ import { useEffect, useState } from "react"
 import { LeftFilters } from "./LeftFilters"
 import { RightProducts } from "./RightProducts"
 import axios from "../../../api/axios"
-import { type ProductData, type ProductFilter } from "../../../types/types"
+import { type ProductData } from "../../../types/types"
+import { useProductFilters } from "../../../contexts/ProductFilterContext"
+import { ProductTopBar } from "./ProductTopBar"
 
 
 
 export const ListProduct = () => {
-  const [filters, setFilters] = useState<ProductFilter>({
-        type: "Shirts",
-        color: "",
-        minPrice: 100,
-        maxPrice: 10000
-  });
+
+  const {filters,searchKeyword} = useProductFilters()
 
   const [products, setProducts] = useState<ProductData[]>([])
 
   useEffect(() => {
     const fetchProducts = async () => {
-        const res = await axios.get('/product',{params: filters})
-        setProducts(res.data.products)
+
+      try{
+        let res
+
+        if(searchKeyword && searchKeyword.trim() !== ""){
+            const encoded = encodeURIComponent(searchKeyword)
+            res = await axios.get(`/product/search?q=${encoded}`,{params: filters})
+            setProducts(res.data.products)
+        }
+        else{
+            res = await axios.get('/product',{params: filters})
+            setProducts(res.data.products)
+        }
+      }
+      catch(error){
+        console.error("Error fetching products:", error);
+      }
+        
     }
 
     fetchProducts()
-  },[filters])
+  },[filters,searchKeyword])
+
+  
 
   return (
     <>
-      <div className="col-xs-24 reset-padding" style={{marginBottom:'50px', marginTop:'50px'}}>
-        <LeftFilters filters={filters} setFilters={setFilters} />
+      <ProductTopBar product={products[0]} />
+      <div className="col-xs-24 reset-padding" style={{marginBottom:'50px'}}>
+        <LeftFilters />
         <RightProducts products ={products} />
       </div>
     </>
