3baef98debb4dbb0b765d730d7ed176f73a49a50
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-22 17:39:55 +0530

    minor changes

--- frontend/src/components/Account/Orders.tsx
+++ frontend/src/components/Account/Orders.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import  { useEffect, useState } from "react"
 import {type OrderType } from "../../types/types"
 import axios from "../../api/axios"
 import { Link } from "react-router-dom"
