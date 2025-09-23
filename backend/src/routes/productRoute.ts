d9f05e66b0371db007cbc5fcbe4cbf72d31af864
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-24 01:40:54 +0530

    Added stock Validation route

--- backend/src/routes/productRoute.ts
+++ backend/src/routes/productRoute.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createProduct, deleteProduct, getProductById, getProducts, searchProducts, updateProduct } from "../controllers/productController";
+import { createProduct, deleteProduct, getProductById, getProducts, searchProducts, updateProduct, validateStock } from "../controllers/productController";
 import { requireAdmin } from "../middleware/AuthMiddleware";
 
 const route = Router()
 
 route.post("/",requireAdmin,createProduct)
+route.post("/validateStock",validateStock)
 route.get("/",getProducts)
 route.get("/search",searchProducts)
 route.get("/:id",getProductById)
