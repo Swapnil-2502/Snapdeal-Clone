277d955dd92f2853fac8dcb0129d2347c2a9ce8b
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-09-23 23:53:24 +0530

    Removed middleware

--- backend/src/routes/productRoute.ts
+++ backend/src/routes/productRoute.ts
@@ -8,7 +8,7 @@ route.post("/",requireAdmin,createProduct)
 route.get("/",getProducts)
 route.get("/search",searchProducts)
 route.get("/:id",getProductById)
-route.patch("/:id",requireAdmin,updateProduct)
+route.patch("/:id",updateProduct)
 route.delete("/:id",requireAdmin,deleteProduct)
 
 export default route
